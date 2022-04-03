5d48abae1ca0e9d24c8d6814aee2f698240bc6e7
Gianfranco Talocchino <gftalocchino@gmail.com>
2022-04-03 09:44:28 -0300

    Se agregan los datos del alumno TALOCCHINO, Gianfranco

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s GIANFRANCO_TALOCCHINO = {
+    .apellidos = "TALOCCHINO",
+    .nombres = "Gianfranco",
+    .documento = "39.215.357",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &GIANFRANCO_TALOCCHINO,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
